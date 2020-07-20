e0862767b10a5ff3685d5d8d6275d11370a1b892
astromgkim <astromgkim@gmail.com>
2020-07-20 18:00:55 +0900

    Update keras_metric.py

--- metric/keras_metric.py
+++ metric/keras_metric.py
@@ -13,3 +13,11 @@ def plv(y_true, y_pred):
         inputs=vgg.input, outputs=vgg.get_layer("block3_conv3").output
     )
     return K.mean(K.square(loss_model(y_true) - loss_model(y_pred)))
+
+
+
+def recall(y_true, y_pred):
+    y_pred = K.round(y_pred)
+    TP = y_true * y_pred
+    FN = y_true * (1-y_pred)
+    return TP / (TP+FN +K.epsilon())
