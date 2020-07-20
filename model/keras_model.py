a1a208bdeef9ada7186eed2cca3f26f6dfbe2507
astromgkim <astromgkim@gmail.com>
2020-07-20 15:29:17 +0900

    Update keras_model.py

--- model/keras_model.py
+++ model/keras_model.py
@@ -52,3 +52,51 @@ def unet(inputs):
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     return conv10
+
+
+
+
+def unet_deep_bottleneck_dilation(inputs):
+    
+    conv0 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv0 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv0)
+    pool0 = MaxPooling2D(pool_size=(2, 2))(conv0)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool0)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    
+    up1 = UpSampling2D(size = (2,2))(conv4)
+    
+    merge1 = concatenate([conv3,up1], axis = 3)
+    conv5 = Conv2D(256, 3, activation = 'relu', padding = 'same', dilation_rate=2, kernel_initializer = 'he_normal')(merge1)
+    conv5 = Conv2D(256, 3, activation = 'relu', padding = 'same', dilation_rate=2, kernel_initializer = 'he_normal')(conv5)
+    
+    up2 = UpSampling2D(size = (2,2))(conv5)
+    merge2 = concatenate([conv2,up2])
+    
+    conv6 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge2)
+    conv6 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+    
+    up3 = UpSampling2D(size = (2,2))(conv6)
+    merge3 = concatenate([conv1,up3])
+    
+    conv7 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge3)
+    conv7 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+    
+    up4 = UpSampling2D(size = (2,2))(conv7)
+    merge4 = concatenate([conv0,up4])
+    
+    conv8 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge4)
+    conv8 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    
+    conv9 = Conv2D(1, 1, activation = 'tanh')(conv8)
+
+    return conv9
