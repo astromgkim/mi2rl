0c2c16122a22ea38b8d1025a541e628dfaf910e0
astromgkim <astromgkim@gmail.com>
2020-07-20 15:50:48 +0900

    Update keras_metric.py

--- metric/keras_metric.py
+++ metric/keras_metric.py
@@ -1,1 +1,15 @@
 
+
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+from tensorflow.keras.applications.vgg19 import VGG19
+
+def plv(y_true, y_pred):
+    # must ensure that y_true and y_pred come from same distribution
+    # nice to have - similar distribution to vgg19 inputs
+    y_pred = tf.image.grayscale_to_rgb(y_pred, name=None) #need to convert as UNet outputs single channel
+    vgg = VGG19(include_top=False, weights="imagenet", input_shape=(512, 512, 3))
+    loss_model = Model(
+        inputs=vgg.input, outputs=vgg.get_layer("block3_conv3").output
+    )
+    return K.mean(K.square(loss_model(y_true) - loss_model(y_pred)))
