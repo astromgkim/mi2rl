8481159cd2f8aa095d07011cccac3e3fd4095aa9
astromgkim <astromgkim@gmail.com>
2020-07-21 09:22:28 +0900

    Update keras_metric.py

--- metric/keras_metric.py
+++ metric/keras_metric.py
@@ -1,20 +1,14 @@
+import keras.backend as K
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
 
+image_shape = (512, 512, 3)
 
-import tensorflow as tf
-tf.compat.v1.disable_eager_execution()
-#from tensorflow.keras.applications.vgg19 import VGG19
-from keras.applications.vgg19 import VGG19
-
-def plv(y_true, y_pred):
-    # must ensure that y_true and y_pred come from same distribution
-    # nice to have - similar distribution to vgg19 inputs
-    y_pred = tf.image.grayscale_to_rgb(y_pred, name=None) #need to convert as UNet outputs single channel
-    vgg = VGG19(include_top=False, weights="imagenet", input_shape=(512, 512, 3))
-    loss_model = Model(
-        inputs=vgg.input, outputs=vgg.get_layer("block3_conv3").output
-    )
+def perceptual_loss(y_true, y_pred):
+    vgg = VGG16(include_top=False, weights='imagenet', input_shape=image_shape)
+    loss_model = Model(inputs=vgg.input, outputs=vgg.get_layer('block3_conv3').output)
+    loss_model.trainable = False
     return K.mean(K.square(loss_model(y_true) - loss_model(y_pred)))
-
 
 
 def recall(y_true, y_pred):
